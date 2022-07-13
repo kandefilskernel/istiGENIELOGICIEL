ef8fa369e16df92daa610190c66b5a92feda9a36
FILS KERNEL KAN <99614495+kandefilskernel@users.noreply.github.com>
2022-07-14 00:17:51 +0200

    Add files via upload
    voir

---
+++ BankAccountService.java
@@ -0,0 +1,34 @@
+package technuru.user.services;
+
+import technuru.user.dtos.*;
+import technuru.user.entities.*;
+import technuru.user.exceptions.BalanceNotSufficientException;
+import technuru.user.exceptions.BankAccountNotFoundException;
+import technuru.user.exceptions.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface BankAccountService {
+     CustomerDTO saveCustomer(CustomerDTO customerDTO);
+     CurrentBankAccountDTO saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+     SavingBankAccountDTO saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+     List<CustomerDTO> listCustomers();
+     BankAccountDTO getBankAccount(String accountId) throws BankAccountNotFoundException;
+     void debit(String accountId,double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+     void credit(String accountId,double amount, String description) throws BankAccountNotFoundException;
+     void transfer(String accountIdSource,String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
+
+     List<BankAccountDTO>bankAccountList();
+
+     CustomerDTO getCustomer(Long customerId) throws CustomerNotFoundException;
+
+     CustomerDTO updateCustomer(CustomerDTO customerDTO);
+
+     void  deleteCustomer(Long customerId);
+
+    List<AccountOperationDTO> accountHistory(String accountId);
+
+     AccountHistoryDTO getAccountHistory(String accountId, int page, int size) throws BankAccountNotFoundException;
+
+    List<CustomerDTO> searchCustomers(String keyword);
+}
